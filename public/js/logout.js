fc253defd7e1bc1609090f5341b2615fb33c3dd0
miseutic <82798984+miseutic@users.noreply.github.com>
2022-08-18 01:20:30 +0800

    Delete public directory

--- public/js/logout.js
+++
@@ -1,7 +0,0 @@
-function logoutMe() {
-    $('#registerMenu').show();
-    $('#loginMenu').show();
-    $('#logoutMenu').hide();
-    $('#editMenu').hide();
-    sessionStorage.removeItem("token");
-}
