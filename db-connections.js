581a3cb1a8decde12ef9ac7cefd261333ad29cc5
miseutic <82798984+miseutic@users.noreply.github.com>
2022-08-18 01:04:57 +0800

    Add files via upload

---
+++ db-connections.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'restaurantreview.cmkfu3uq9bax.us-east-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'admin',
+    password: '386462291955Is',
+    database: '-'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected to DB');
+});
+module.exports = connection;
